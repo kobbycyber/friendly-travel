5bf5bbf687cac0eb9f73e059583322626d35ea73
Moa Granberg <moa.granberg@hotmail.com>
2020-12-15 22:38:15 +0100

    Style header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Head from 'next/head';
 import '../styles/globals.scss';
+import styles from './App.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,13 +9,19 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Friendly Travel</title>
         <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Yeseva+One&display=swap" rel="stylesheet"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat&family=Yeseva+One&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
 
-      <header>
-        <nav>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
           <Link href="/">
-            <a>Home</a>
+            <a className={styles.homeLink}>
+              <h3>Friendly Travel</h3>
+              <img src={'/logo.svg'} alt="logo" />
+            </a>
           </Link>
           <Link href="/trips/">
             <a>Trips</a>
