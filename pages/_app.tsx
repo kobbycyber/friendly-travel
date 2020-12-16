a2d47025e10a7840995c745f8dec5253cf5fe5ed
Moa Granberg <moa.granberg@hotmail.com>
2020-12-16 11:10:23 +0100

    Layout header - footer - main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,9 @@ function MyApp({ Component, pageProps }) {
         </nav>
       </header>
 
-      <Component {...pageProps} />
+      <main>
+        <Component {...pageProps} />
+      </main>
 
       <footer className={styles.footer}>
         <Link href="/">
