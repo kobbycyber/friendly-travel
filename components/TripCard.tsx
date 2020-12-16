a7a9d0a1b688eae30bd8ab4a37b267bf7d1a9270
Moa Granberg <moa.granberg@hotmail.com>
2020-12-16 14:01:52 +0100

    Template TripCard

--- components/TripCard.tsx
+++ components/TripCard.tsx
@@ -1,19 +1,33 @@
-import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import Link from 'next/link';
+
+import buttonStyles from '../styles/buttons.module.scss';
 
 interface TripCardProps {
-  title: string,
-  startDate: string,
-  endDate: string,
-  imageUrl: string,
+  title: string;
+  startDate: string;
+  endDate: string;
+  imageUrl: string;
 }
 
-function TripCard({ title, startDate, endDate, imageUrl }: TripCardProps) {  
+function TripCard({ title, startDate, endDate, imageUrl }: TripCardProps) {
+  const reformatDate = (start: string, end: string) => {
+    return '3 - 5 maj';
+  };
+
+  const getSlug = (title: string) => {
+    return 'san-fransisco';
+  };
+  const slug = getSlug(title);
+
   return (
-    <div className="container">
-      {/* <img src={imageUrl} /> */}
-      <div className="text">
+    <div>
+      <img src={imageUrl} />
+      <div>
         <h2>{title}</h2>
-        {/* <h4>{endDate}</h4> */}
+        <p>{reformatDate(startDate, endDate)}</p>
+        <Link href={`/trips/${slug}`}>
+          <a className={buttonStyles.primaryButton}>Read more</a>
+        </Link>
       </div>
     </div>
   );
